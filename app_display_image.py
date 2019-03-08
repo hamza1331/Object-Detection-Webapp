396610fad43670f08b7f506e309aead38d1a127d
Hamza Ali <hamxa1331@gmail.com>
2019-03-09 02:57:50 +0500

    Initial Commit
    Basic flask app implemented for object detection. (No Styling).

---
+++ app_display_image.py
@@ -0,0 +1,52 @@
+import os
+from imageai.Detection import ObjectDetection
+from uuid import uuid4
+
+from flask import Flask, request, render_template, send_from_directory
+
+__author__ = 'ibininja'
+
+app = Flask(__name__)
+# app = Flask(__name__, static_folder="images")
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    # target = os.path.join(APP_ROOT, 'static/')
+    print(target)
+    if not os.path.isdir(target):
+            os.mkdir(target)
+    else:
+        print("Couldn't create upload directory: {}".format(target))
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        destination = "/".join([target, filename])
+        print ("Accept incoming file:", filename)
+        print ("Save it to:", destination)
+        upload.save(destination)
+
+    # return send_from_directory("images", filename, as_attachment=True)
+    execution_path = target
+    print(execution_path)
+    detector = ObjectDetection()
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath( os.path.join(execution_path ,"resnet50_coco_best_v2.0.1.h5"))
+    detector.loadModel()
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path ,filename), output_image_path=os.path.join(execution_path , "flask"+filename))
+    return render_template("complete_display_image.html", image_name="flask"+filename)
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+if __name__ == "__main__":
+    app.run(port=4555, debug=True)
